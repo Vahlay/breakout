880f0f2dc4f04072a82f641fb49f076add7619e7
Vahic Laydier <vahic@laydier.fr>
2019-09-09 23:54:00 +0200

    First commit : vue-cli files with a few fixes

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Vue, {CreateElement, VNode} from 'vue';
+import App from './App.vue';
+import store from './store';
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  vuetify,
+  render: (h: CreateElement): VNode => h(App),
+}).$mount('#app');
