7d1ed7600d36cd720294f513864467dc16665a25
Vahic Laydier <vahic@laydier.fr>
2019-09-28 21:51:43 +0200

    Adds the player's bar

--- src/components/GameArea.vue
+++ src/components/GameArea.vue
@@ -1,20 +1,40 @@
 <template>
-    <div>
-
-        <v-btn fab color="pink"><v-icon>mdi-heart</v-icon></v-btn>
-
-
+    <div class="game-area">
+        <div class="player-bar-container">
+            <player-bar/>
+        </div>
     </div>
+    
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
+    import PlayerBar from './PlayerBar.vue';
 
     export default Vue.extend({
         name: 'GameArea',
+        components: {
+            PlayerBar,
+        },
+        methods: {
+        },
     });
 </script>
 
 <style scoped>
+    .game-area{
+        background-color: rgba(150,150,0,0.5);
+        width   : 100%;
+        height  : 100%;
 
+        display: flex;
+        flex-direction: column
+    }
+
+    .player-bar-container {
+        margin-top: auto;
+        margin-bottom: 1%;
+        width: 100%;
+        height: 5%;
+    }
 </style>
---
+++ src/components/PlayerBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="player-bar">
+</div>
+
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'PlayerBar',
+        methods: {
+        },
+    });
+</script>
+
+<style scoped>
+    .player-bar{
+        width: 15%;
+        height: 100%;
+        background-color: #000;
+    }
+</style>
