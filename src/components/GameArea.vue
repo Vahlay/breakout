d4f8d2c8159aa9bacbcf5303bc28d63c069c96a7
Vahic Laydier <vahic@laydier.fr>
2019-09-30 23:58:15 +0200

    Feeds all the keyup & keydown event to a global event bus

--- src/components/GameArea.vue
+++ src/components/GameArea.vue
@@ -24,7 +24,7 @@
         components: {
             PlayerBar,
             Brick,
-            Ball
+            Ball,
         },
         methods: {
         },
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import Vue, {CreateElement, VNode} from 'vue';
 import App from './App.vue';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import {KeyboardEventBus} from './KeyboardEventBus';
 
 Vue.config.productionTip = false;
 
+document.addEventListener('keyup', (event) => {
+  KeyboardEventBus.$emit('keyup', event);
+});
+
+document.addEventListener('keydown', (event) => {
+  KeyboardEventBus.$emit('keydown', event);
+});
 
 new Vue({
   store,
